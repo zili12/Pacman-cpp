03b9238c33f414989a11eb9087fe0ce12e2979ea
Ron Zilberberg <93860114+zili12@users.noreply.github.com>
2022-05-12 16:24:23 +0300

    Update io_utils.h

--- io_utils.h
+++ io_utils.h
@@ -1,7 +1,6 @@
 #ifndef _IO_UTILS_H_
 #define _IO_UTILS_H_
-// you must have a config file, both for windows and for compiling on Linux (MAMA)
-// BUT, you need to change the content of the config!
+
 #include "config.h"
 #include "Enum.h"
 #include <iostream>
